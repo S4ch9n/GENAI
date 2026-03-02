caea12899cdcc46219fdf4c0b2dec3ff01e94be4
iam s4ch9n <sunnythakur666556@gmail.com>
2026-03-03 00:41:39 +0530

    pydanticOutputParser

--- OutputParser/pydanticoutputparser.py
+++ OutputParser/pydanticoutputparser.py
@@ -3,34 +3,33 @@ from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-
-load_dotenv()
-
-# Step 1: Define Output Schema
+import os
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 class CountryInfo(BaseModel):
     capital: str = Field(description="Capital of the country")
     population: str = Field(description="Population of the country")
-
-# Step 2: Create Parser
+    
 parser = PydanticOutputParser(pydantic_object=CountryInfo)
 
-# Step 3: LLM
 llm = ChatGroq(model="llama-3.1-8b-instant")
 
-# Step 4: Prompt Template
 prompt = PromptTemplate(
-    template="Give capital and population of India.\n{format_instructions}",
+    template="""
+Give capital and population of India.
+
+Return ONLY valid JSON.
+Do NOT explain.
+Do NOT add text.
+Do NOT add markdown.
+
+{format_instructions}
+""",
     input_variables=[],
-    partial_variables={
-        "format_instructions": parser.get_format_instructions()
-    }
+    partial_variables={"format_instructions": parser.get_format_instructions()},
 )
 
-# Step 5: Chain
 chain = prompt | llm | parser
 
-# Step 6: Invoke
 result = chain.invoke({})
 
 print(result)
-print(type(result))
